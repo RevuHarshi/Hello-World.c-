de45f8191c3f64d6501cde73c11a430727875072
RevuHarshi <atrevathiharshini@gmail.com>
2022-02-17 07:43:38 +0000

    commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Eggs
+    {
+        public void DisplayOne()
+        {
+            Console.WriteLine("Eggs");
+        }
+    }
+    class Caterpillar : Eggs
+    {
+        public void DisplayTwo()
+        {
+            Console.WriteLine("caterpillar");
+        }
+    }
+    class Butterfly : Caterpillar
+    {
+        public void DisplayThree()
+        {
+            Console.WriteLine("Butterfly");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Butterfly b = new Butterfly();
+            b.DisplayOne();
+            b.DisplayTwo();
+            b.DisplayThree();
+
+            Console.ReadKey();
+                
+        }
+    }
+}
